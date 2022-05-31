451cf6f98365f046b63c52cf4959168416a4f282
jjignacio <you@example.com>
2022-05-30 21:36:43 -0300

    fix heroku error

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,5 +1,11 @@
 export class createUserDTO {
     name: string;
     password: string;
+    alias: string;
+    enabled: boolean;
+    email: string;
+    role: string;
+    gender: string;
+    birth: string;
     createdAt: Date;
 }
--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -1,10 +1,18 @@
 import { Schema } from "mongoose";
 
 export const UserSchema = new Schema ({
+
     name: { type: String, required: true },
     password: String,
+    alias: { type: String, required: true },
+    enabled: { type: Boolean, required: true },
+    email: { type: String, required: true },
+    role: { type: String, required: true },
+    gender: { type: String, required: true },
+    birth: { type: String, required: true },
     createdAt: {
         type: Date,
         default: Date.now
     }
+    
 })
