d7e2ee4f60126428b54c7f5d13822e95bacb0272
jjignacio <you@example.com>
2022-06-04 18:31:19 -0300

    Mongo Atlas configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,8 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest-appetito'), UsersModule],
+  //imports: [MongooseModule.forRoot('mongodb://localhost/nest-appetito'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://appetito:3LD44rq5b91UuQ2c@cluster0.9bfr6jt.mongodb.net/?retryWrites=true&w=majority'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
