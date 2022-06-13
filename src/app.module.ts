a6205b6348e696393da8ba96f45bc913fcf70b40
jjignacio <you@example.com>
2022-06-13 18:45:40 -0300

    login v1.1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from "@nestjs/mongoose";
 import { UsersModule } from './users/users.module';
-import { LoginModule } from './login/login.module';
 
 @Module({
   //imports: [MongooseModule.forRoot('mongodb://localhost/nest-appetito'), UsersModule],
-  imports: [MongooseModule.forRoot('mongodb+srv://appetito:3LD44rq5b91UuQ2c@cluster0.9bfr6jt.mongodb.net/?retryWrites=true&w=majority'), UsersModule, LoginModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://appetito:3LD44rq5b91UuQ2c@cluster0.9bfr6jt.mongodb.net/?retryWrites=true&w=majority'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
