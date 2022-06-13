0a1ac1ff3aa9f071cf5e6d8fed70be479d450810
jjignacio <you@example.com>
2022-06-13 20:35:16 -0300

    createUser v1.1

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -2,14 +2,14 @@ import { Schema } from "mongoose";
 
 export const UserSchema = new Schema ({
 
-    name: { type: String, required: true },
+    name: { type: String },
     password: String,
     alias: { type: String, required: true },
-    enabled: { type: Boolean, required: true },
+    enabled: { type: Boolean },
     email: { type: String, required: true },
-    role: { type: String, required: true },
-    gender: { type: String, required: true },
-    birth: { type: String, required: true },
+    role: { type: String },
+    gender: { type: String },
+    birth: { type: String },
     createdAt: {
         type: Date,
         default: Date.now
