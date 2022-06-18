a1187a3872519ef9c70e78d95c1cb43e69875a96
jjignacio <you@example.com>
2022-06-18 13:16:26 -0300

    Recover Password V1.5

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -84,7 +84,7 @@ export class UsersService {
     async recoverPassword(userEmail, postData): Promise<User>  {
         var user = await this.userModel.findOne({ email: userEmail });
         if(user.enabled && user.role.localeCompare("Invitado") == 0) {
-            user = await this.userModel.findOneAndUpdate({ password: postData.password });
+            user = await this.userModel.findOneAndUpdate({email: userEmail}, { password: postData.password });
             return user;
         }
     }
