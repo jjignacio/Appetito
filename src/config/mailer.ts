6f35742520ae348ff9638507579a79405a7f5eb7
jjignacio <you@example.com>
2022-06-20 00:04:34 -0300

    Forgot Password V.1.5

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -2,9 +2,9 @@
   
   // create reusable transporter object using the default SMTP transport
   export const transporter = nodemailer.createTransport({
-    host: "Gmail",
-    port: 25,
-    //secure: true, // true for 465, false for other ports
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
       user: 'marquezjuan2211@gmail.com', // generated ethereal user
       pass: 'bcoavzgrbfmdnthm', // generated ethereal password
