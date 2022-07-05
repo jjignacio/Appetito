d6fd469bd1bf1dff9ccaeee42cccf7658a466e57
jjignacio <you@example.com>
2022-07-05 20:02:53 -0300

    Reviews  v.3.3

--- src/recetas/recetas.service.ts
+++ src/recetas/recetas.service.ts
@@ -102,19 +102,14 @@ export class RecetasService {
     }*/
 
     async createReview(Id: string, postData): Promise<Recetas> {
-
-        // 1. Get the product's Comments array of comment ids.
-        const calificaciones = await this.recetasModel.findOne({_id: Id}, { $group: { avgQuantity: { $avg: "$reviews.calificacion" }}});
-
-        /*const calificaciones = await this.recetasModel.aggregate([
-            { $group: 
-                {
-                    _id : Id , 
-                    avgQuantity: { $avg: "$reviews.calificacion" }
-                }
-            }
-        ]);*/
-        const puntuacion = calificaciones[0].avgQuantity;
+        const receta = await this.recetasModel.findOne({_id: Id});
+        var suma = 0;
+        var contador = 0;
+        receta.reviews.forEach((review) => {
+            suma = suma + review.calificacion;
+            contador++;
+        });
+        const puntuacion = suma / contador;
         await this.recetasModel.findOneAndUpdate({_id: Id}, { $push: {"reviews": {calificacion: postData.calificacion, comentario: postData.comentario}}});
         const updatedReceta = await this.recetasModel.findOneAndUpdate({_id: Id}, { puntuacion: puntuacion}, {new : true});
         return updatedReceta;
